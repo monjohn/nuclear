7bc3a03d22b1abfa263c6089355e6fd83ef8decf
Monte Johnston <johnston.monte@gmail.com>
2023-08-07 15:55:29 -0400

    initial commit

---
+++ serum.exs
@@ -0,0 +1,11 @@
+%{
+  site_name: "NC for Nuclear",
+  site_description: "Promoting nuclear power in North Carolina",
+  date_format: "{WDfull}, {D} {Mshort} {YYYY}",
+  base_url: "/",
+  author: "author",
+  author_email: "john.doe@example.com",
+  plugins: [
+    {Serum.Plugins.LiveReloader, only: :dev}
+  ]
+}
